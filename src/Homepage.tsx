9ce2802947535b918edbb71feb6c2766fdfd97dc
Hyunjin Lee <hyunjinwaterloo@gmail.com>
2024-05-21 12:38:28 -0400

    removed unnecessary commented code

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -14,52 +14,6 @@ export default function Homepage() {
   const [displayTemp, setDisplayTemp] = useState(0);
 
   useEffect(() => {
-    // setWeatherData({
-    //   coord: {
-    //     lon: -79.4163,
-    //     lat: 43.7001
-    //   },
-    //   weather: [
-    //     {
-    //       id: 800,
-    //       main: 'Clear',
-    //       description: 'clear sky',
-    //       icon: '01d'
-    //     }
-    //   ],
-    //   base: 'stations',
-    //   main: {
-    //     temp: 20.39,
-    //     feels_like: 19.89,
-    //     temp_min: 17.4,
-    //     temp_max: 23.06,
-    //     pressure: 1011,
-    //     humidity: 54
-    //   },
-    //   visibility: 10000,
-    //   wind: {
-    //     speed: 4.63,
-    //     deg: 70
-    //   },
-    //   clouds: {
-    //     all: 0
-    //   },
-    //   dt: 1715877669,
-    //   sys: {
-    //     type: 2,
-    //     id: 2043365,
-    //     country: 'CA',
-    //     sunrise: 1715853059,
-    //     sunset: 1715906239
-    //   },
-    //   timezone: -14400,
-    //   id: 6167865,
-    //   name: 'Toronto',
-    //   cod: 200
-    // });
-
-    // Actual API call logic. Use hardcorded response above for testing to save on
-    // API usage (limited to 1000 calls per day)
     const fetchDataForPosts = async () => {
       try {
         setError(false);
